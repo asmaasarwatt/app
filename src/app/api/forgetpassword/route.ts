a03488bd6b9f5c800ab781917e5040d4a42bdbbf
asmaasarwatt <asmaasarwat31@gmail.com>
2026-02-20 23:52:49 +0200

    new

--- src/app/api/forgetpassword/route.ts
+++ src/app/api/forgetpassword/route.ts
@@ -1,25 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    const res = await fetch(
-      "https://ecommerce.routemisr.com/api/v1/auth/forgotPasswords",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: body.email }),
-      }
-    );
-
-    const data = await res.json();
-
-    if (data.statusMsg === "fail") {
-      return NextResponse.json({ error: data.message }, { status: 400 });
+import { getToken } from "next-auth/jwt";
+export async function GET(req:NextRequest){
+    const token= await  getToken({req})
+   
+    if(!token){
+       return NextResponse.json({error:'unauthorized', status:401})
     }
-
-    return NextResponse.json({ message: "Email sent successfully" });
-  } catch (err) {
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
-  }
+    const rep = await fetch(`${process.env.API}/forgetpasswords`,{
+        headers:{
+            token:token.token ,
+            'Content-type':'application/json'
+        }
+    })
+    const payload= await rep.json()
+    return NextResponse.json(payload)
 }
